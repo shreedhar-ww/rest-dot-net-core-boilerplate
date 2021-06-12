3f5d1034326ba5f8d2caf6db26390154905d7ad9
vatsal2110 <49248727+vatsal2110@users.noreply.github.com>
2021-06-12 12:23:34 +0530

    Add project files.

---
+++ HotelListing/Models/CountryDTO.cs
@@ -0,0 +1,24 @@
+﻿using HotelListing.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.Models
+{
+    public class CreateCountryDTO
+    {
+        [Required]
+        [StringLength(maximumLength: 150, ErrorMessage = "Country name is too long.")]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(maximumLength: 3, ErrorMessage = "Short Country name is too long.")]
+        public string ShortName { get; set; }
+    }
+    public class CountryDTO : CreateCountryDTO
+    {
+        public int Id { get; set; }
+        public IList<Hotel> Hotels { get; set; }
+    }
+}
